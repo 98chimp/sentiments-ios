f5b4513169547e010fbca7248bfb230d82e5fb12
Shahin Zangenehpour <shahin@98chimp.com>
2018-11-10 16:57:02 -0800

    Initial Commit

---
+++ Sentiments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sentiments
+//
+//  Created by Shahin on 2018-11-10.
+//  Copyright © 2018 98%Chimp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SentimentsTests/SentimentsTests.swift
@@ -0,0 +1,34 @@
+//
+//  SentimentsTests.swift
+//  SentimentsTests
+//
+//  Created by Shahin on 2018-11-10.
+//  Copyright © 2018 98%Chimp. All rights reserved.
+//
+
+import XCTest
+@testable import Sentiments
+
+class SentimentsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
